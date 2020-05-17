0793ddd543fff152806d3a3039b5873e37cbc5a4
suhel hasan <suhelhasan202@gmail.com>
2020-05-17 18:54:48 +0530

    Add CSS to the Login page
    Signed-off-by: suhel hasan <suhelhasan202@gmail.com>

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,35 +1,65 @@
-import React,{Component} from 'react';
-import './Login.css';
+import React, { Component } from "react";
+import styling from "./Login.module.css";
+import logo from "./gestures.svg";
 
-class Login extends Component{
+class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
 
-    state={
-        email:"",
-        password:""
-    }
+  emailChangeHandler = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  passwordChangeHandler = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
 
-    emailChangeHandler=(e)=>{
-        this.setState({
-            email:e.target.value
-        })
-    }
-    passwordChangeHandler=(e)=>{
-        this.setState({
-            password:e.target.value
-        })
-    }
-
-    render(){
-        return (
-        <div className="login">
-            <h1>This is login</h1>
-            <form>
-                <input type="text" placeholder="Enter email address" onChange={this.emailChangeHandler} value={this.state.email}/>
-                <input type="password" placeholder="Enter Password" onChange={this.passwordChangeHandler} value={this.state.password}/>
-                <input type="submit" value="Login" onClick={(e)=>this.props.onLoginHandler(e,this.state.email,this.state.password)}/>
-            </form>
+  render() {
+    return (
+      <div className={styling.loginPage}>
+        <div className={styling.login}>
+          <div className={styling.header}>
+            <img alt="logo" src={logo} />
+            <h1>Hello, </h1>
+            <h3>Please Login</h3>
+          </div>
+          <form>
+            <input
+              type="text"
+              placeholder="Enter email address"
+              onChange={this.emailChangeHandler}
+              value={this.state.email}
+            />
+            <input
+              type="password"
+              placeholder="Enter Password"
+              onChange={this.passwordChangeHandler}
+              value={this.state.password}
+            />
+            <div className={styling.checkboxDiv}>
+              <input type="checkbox" value="" />
+              <label> Remember Me</label>
+            </div>
+            <input
+              type="submit"
+              value="Login"
+              onClick={(e) =>
+                this.props.onLoginHandler(
+                  e,
+                  this.state.email,
+                  this.state.password
+                )
+              }
+            />
+          </form>
         </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 export default Login;
